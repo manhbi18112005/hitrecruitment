80dfdce202c5b387672619b73ded92a5eefeefc8
๖ۣۜŇŇ_MyTツ <myt@nonamestudio.xyz>
2021-09-13 14:02:20 +0700

    Create b3.cpp

---
+++ Day 2/B3/b3.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fr(name) freopen(name, "r", stdin);
+#define fw(name) freopen(name, "w", stdout);
+
+void solve() {
+    int n;
+    cin >> n;
+}
+int main() {
+    solve();
+    return 0;
+}
