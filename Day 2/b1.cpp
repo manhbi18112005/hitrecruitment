affe4c9ab19598d350e2b2a151d9640fbea1dae6
๖ۣۜŇŇ_MyTツ <myt@nonamestudio.xyz>
2021-09-13 12:40:35 +0700

    B1 - D2

---
+++ Day 2/b1.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fr(name) freopen(name, "r", stdin);
+#define fw(name) freopen(name, "w", stdout);
+
+int Q;
+void solve() {
+    cin >> Q;
+    int dem[Q];
+    //  fr("PRIME.INP");
+    int L[Q], R[Q];
+    for(int i = 1; i <= Q; i++) {
+        int a, b;
+        cin >> a >> b;
+        L[i] = a;
+        R[i] = b;
+        dem[i] = 0;
+    }
+    for(int i = 1; i <= Q; i++) {
+        int tong = 0;
+        int a, b;
+        a = L[i]; //20
+        b = R[i]; //30
+        int f = b-a+1; //10
+        for(int j = a; j <= b; j++) {
+            for(int k = 2; k < b; k++) {
+                if(j % k == 0) continue;
+            }
+            tong++;
+        }
+        dem[i] = tong;
+    }
+    for(int i = 1; i <= Q; i++) {
+        cout << dem[i] << endl;
+    }
+}
+
+int main() {
+    solve();
+
+}
