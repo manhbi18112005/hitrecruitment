884968f6b4bccb2a8e1076c008e128b1d12e645d
๖ۣۜŇŇ_MyTツ <myt@nonamestudio.xyz>
2021-09-13 16:35:18 +0700

    final

--- Day 1/B2/b2.cpp
+++ Day 1/B2/b2.cpp
@@ -1,39 +1,39 @@
-#include <iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
-long long ch(int x)
-{
-   long long a=1;
-   for(int i=2;i<=x;i++)
-   {
-       a=a[i];
+#define fr(name) freopen(name, "r", stdin);
+#define fw(name) freopen(name, "w", stdout);
+
+int n;
+long long check(int x) {
+   long long a = 1;
+   for(int i = 2; i <= x; i++) {
+       a = a * i;
    }
    return(a);
 }
-int main()
-{
-    int n;
+void solve() {
     int s[100000];
     int t,a;
     a=0;
     cin >> n;
-    for(int i=1;i<=n;i++)
-    {
-            cin >>  t;
-            if(a < t)
-                a=t;
-            s[t]++;
+    for(int i=1;i<=n;i++) {
+        cin >> t;
+        if(a < t)
+        a=t;
+        s[t]++;
     }
     long long d;
     d=0;
-    for(int i=1;i<=a;i++)
-    {
-            if(s[i]>=2)
-                    {
-                        long long c;
-                        c=ch(s[i])/(2ch(s[i]-2));
-                        d=(d+c);
-                    }
+    for(int i=1;i<=a;i++) {
+        if(s[i]>=2) {
+            long long c;
+            c=check(s[i])/(2*check(s[i]-2));
+            d=(d+c);
+        }
     }
     cout << d;
+}
+int main() {
+    solve();
+    return 0;
 }
