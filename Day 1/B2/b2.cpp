68cd851bc3d459b62259aa710f2a287b84ee182c
๖ۣۜŇŇ_MyTツ <myt@nonamestudio.xyz>
2021-09-13 13:54:02 +0700

    next

--- Day 1/B2/b2.cpp
+++ Day 1/B2/b2.cpp
@@ -5,9 +5,24 @@ using namespace std;
 
 
 void solve() {
-    fr("MOON.INP");
-    fw("MOON.OUT");
     int n;
+    cin >> n;
+    int a[n];
+    int cap = 0;
+    for(int i = 1; i <= n; i++) {
+        int heh;
+        cin >> heh;
+        a[i] = heh;
+    }
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            if(a[i] == a[j]) {
+                cap++;
+                cout << a[i];
+            }
+        }
+    }
+    cout << cap;
 }
 int main() {
     solve();
---
+++ Day 2/B4/b4.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fr(name) freopen(name, "r", stdin);
+#define fw(name) freopen(name, "w", stdout);
+
+void solve() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    int tong = n;
+    for(int i = 1; i < m; i++) {
+        tong = tong * n;
+    }
+    tong = tong % k;
+    cout << tong;
+}
+int main() {
+    solve();
+}
