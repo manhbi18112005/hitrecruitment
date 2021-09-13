4370e4515d8610086353d148dda064a4532900e9
๖ۣۜŇŇ_MyTツ <myt@nonamestudio.xyz>
2021-09-13 13:09:43 +0700

    a

---
+++ Day 1/B2/b2.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fr(name) freopen(name, "r", stdin);
+#define fw(name) freopen(name, "w", stdout);
+
+
+void solve() {
+    fr("MOON.INP");
+    fw("MOON.OUT");
+    int n;
+}
+int main() {
+    solve();
+}
